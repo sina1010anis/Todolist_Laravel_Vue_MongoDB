cc8e1c7293eb243d711c7cbe1c40e5fdb9c83c0c
sina1010anis <sina1010anis@gmail.com>
2023-05-02 19:01:30 +0330

    fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,9 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\MongoDB\UserMongo;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
